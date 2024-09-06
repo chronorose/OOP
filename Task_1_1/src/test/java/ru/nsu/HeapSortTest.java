b86d656dd3bd5e9f0d8176e40cdf2ec12ddb88e2
rendemare <rendemare@gmail.com>
2024-09-06 21:37:32 +0700

    fixed typo :)

--- Task_1_1/src/test/java/ru/nsu/HeapSortTest.java
+++ Task_1_1/src/test/java/ru/nsu/HeapSortTest.java
@@ -10,6 +10,6 @@ class HeapSortTest {
         int[] arr = new int[]{4, 7, 8, 10, 3, 7, 10};
         int[] sortedArr = new int[]{3, 4, 7, 7, 8, 10, 10};
         HeapSort.heapsort(arr);
-        assertArrayEquals(arr, sorted_arr);
+        assertArrayEquals(arr, sortedArr);
     }
 }
