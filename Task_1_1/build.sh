17438c02b4658506aef240fe9169d7a3f31eed0f
rendemare <rendemare@gmail.com>
2024-09-08 16:49:02 +0700

    fix of build script

--- Task_1_1/build.sh
+++ Task_1_1/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # i do not really have main methods anywhere since it's utility/library type of lab. so no running from shell
-javac -d ./build/classes/java/main/ru/nsu ./src/main/java/ru/nsu/*.java 
+javac -d ./build/classes/java/main/ ./src/main/java/ru/nsu/*.java 
 java -classpath .:build/classes/java/main/ ru.nsu.Main
 javadoc -d ./docs -sourcepath ./src/main/java ru.nsu
 
