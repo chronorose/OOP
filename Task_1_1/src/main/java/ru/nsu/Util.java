4f9e8d19e55a20477a22b184af206e930cbdcc92
rendemare <rendemare@gmail.com>
2024-09-08 13:32:13 +0700

    extra checkstyle fixes

--- Task_1_1/src/main/java/ru/nsu/Util.java
+++ Task_1_1/src/main/java/ru/nsu/Util.java
@@ -11,7 +11,9 @@ public class Util {
      * @param i     first one to be swapped
      * @param j     second one to be swapped
      * @throws NullPointerException           when passed null into {@code array}.
-     * @throws ArrayIndexOutOfBoundsException when elements in indices {@code i} or {@code j} are not present in the array.
+     * @throws ArrayIndexOutOfBoundsException when elements in indices
+     *                                        {@code i} or {@code j}
+     *                                        are not present in the array.
      */
     static void swap(int[] array, int i, int j) {
         array[i] ^= array[j];
