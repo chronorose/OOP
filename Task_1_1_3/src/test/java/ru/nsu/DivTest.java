e7aa419d88d696e9995b0c2ff3cd2ea3ecf5e9b9
rendemare <rendemare@gmail.com>
2024-10-08 10:23:29 +0700

    few more tests

---
+++ Task_1_1_3/src/test/java/ru/nsu/DivTest.java
@@ -0,0 +1,61 @@
+package ru.nsu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class DivTest {
+    @Test
+    void testToString() {
+        Expression e = new Div(new Number(1), new Number(2));
+        assertEquals("(1 / 2)", e.toString());
+    }
+
+    @Test
+    void copyTest() {
+        Expression e = new Div(new Number(1), new Number(2));
+        Expression e2 = e.copy();
+        assertTrue(e.equals(e2));
+    }
+
+    @Test
+    void copyTest2() {
+        Expression e = new Div(new Number(1), new Variable("hoho"));
+        Expression e2 = e.copy();
+        assertTrue(e.equals(e2));
+    }
+
+
+    @Test
+    void testToString2() {
+        Expression e = new Div(new Variable("hihihaha"), new Number(2));
+        assertEquals("(hihihaha / 2)", e.toString());
+    }
+
+    @Test
+    void DivEval1() {
+        Expression e = new Div(new Number(1), new Number(2));
+        assertEquals(0, e.eval(""));
+    }
+
+    @Test
+    void DivEval2() {
+        Expression e = new Div(new Variable("hihi"), new Number(2));
+        assertEquals(1, e.eval("hihi = 3"));
+    }
+
+    @Test
+    void DivSimplify1() {
+        Expression e = new Div(new Number(0), new Mul(new Number(2), new Variable("haha")));
+        Expression simple = e.simplify();
+        assertEquals(0, simple.eval());
+    }
+
+    @Test
+    void DivSimplify2() {
+        Expression e = new Div(new Sub(new Variable("haha"), new Variable("haha")), new Number(100));
+        Expression simple = e.simplify();
+        assertEquals(0, simple.eval());
+    }
+}
