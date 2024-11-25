9391364bd42165d5f0d527b0dcd9ea3b21f01d7c
rendemare <rendemare@gmail.com>
2024-11-25 21:39:34 +0700

    oops got graph task in there(

--- Task_1_1_3/src/main/java/ru/nsu/WrongVariableException.java
+++ Task_1_1_3/src/main/java/ru/nsu/WrongVariableException.java
@@ -2,10 +2,16 @@ package ru.nsu;
 
 class WrongVariableException extends RuntimeException {
     String reason;
+    Throwable cause;
 
     public WrongVariableException() {}
 
     public WrongVariableException(String reason) {
         this.reason = reason;
+    }
+
+    public WrongVariableException(String reason, Throwable cause) {
+        this.reason = reason;
+        this.cause = cause;
     }
 }
--- Task_1_2_1/src/main/java/ru/nsu/Graph.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu;
-
-public interface Graph {
-    void addVertex(Integer vertex);
-    void removeVertex(Integer vertex);
-    void addEdge(Integer from, Integer to);
-    void removeEdge(Integer from, Integer to);
-    Integer[] getNeighbours(Integer vertex);
-}
